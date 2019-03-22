28c9b978e2ee059f6997428c44b22fe6cd0f0798
Antavio <njuguna13@gmail.com>
2019-03-22 15:38:40 +0300

    Add actualDate property to the constructor class.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDesc:boolean;
-    constructor(public id:number, public quote:string,public author:string, public publisher:string,public newDate:Date){
+    constructor(public id:number, public quote:string,public author:string, public publisher:string,public actualDate:Date){
         this.showDesc=false
     }
     
