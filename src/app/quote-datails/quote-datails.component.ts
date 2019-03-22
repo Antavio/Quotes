25bca1b7f76c0780d8c1471164c561c0502f08ec
Antavio <njuguna13@gmail.com>
2019-03-22 12:36:58 +0300

    created quote details component.

---
+++ src/app/quote-datails/quote-datails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-datails',
+  templateUrl: './quote-datails.component.html',
+  styleUrls: ['./quote-datails.component.scss']
+})
+export class QuoteDatailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
