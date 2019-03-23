dd65fdf01f1a71190277cdf2383a1a8d99cb7035
Antavio <njuguna13@gmail.com>
2019-03-23 13:36:09 +0300

    Add a Quote form to enable users input a quote.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDatailsComponent } from './quote-datails/quote-datails.component';
 import { DateRangePipe } from './date-range.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDatailsComponent,
-    DateRangePipe
+    DateRangePipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
